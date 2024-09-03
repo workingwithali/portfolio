bd2cf4ba988c6940ef983543e9fa00f5ac1097c7
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-03 13:03:25 +0500

    update

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,35 @@
-
-
+'use client';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+const links = [
+  {
+    name: "home",
+    path: "/"
+  },
+  {
+    name: "services",
+    path: "/services"
+  },
+  {
+    name: "resume",
+    path: "/resume"
+  },
+  {
+    name: "work",
+    path: "/work"
+  },
+  {
+    name: "contact",
+    path: "/contact"
+  }
+]
 const Nav = () => {
   return (
-    <nav>desltp nav</nav>
+    <nav className="flex gap-8">
+      {links.map((link ,index)=>{
+        <Link href={link.path} key={index}>{link.name}</Link>
+      })}
+    </nav>
   )
 }
 

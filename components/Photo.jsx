fafa70115b5d63a2f618ac8d40e634cbd741282d
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-08 16:55:21 +0500

    update

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -2,15 +2,18 @@
 import { motion } from "framer-motion"
 import Image from "next/image"
 const Photo = () => {
-  return (
-    <div className="w-ful h-ful relative">
-        <motion.div>
-            <motion.div className="w-[298px] h-[298px] xl:w-[]">
-                <Image src="/assets/photo.png" alt="" priority quality={100} fill className="object-contain" />
+    return (
+        <div className="w-ful h-ful relative">
+            <motion.div initial={{opacity:0}} animate={{
+                opacity:1,
+                transition: {delay:2, duration:0.4, ease:"easeIn" }
+            }} > 
+                <motion.div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
+                    <Image src="/assets/photo.png" alt="" priority quality={100} fill className="object-contain" />
+                </motion.div>
             </motion.div>
-        </motion.div>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Photo

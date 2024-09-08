32dad95ea5f661435f6bdec3b309ef5209ac2ab0
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-08 13:12:19 +0500

    update

--- app/page.jsx
+++ app/page.jsx
@@ -9,11 +9,12 @@ const Home = () => {
             <span>Software Engineer</span>
             <h1 className="h1">Hello I'm <br /><span className="text-accent">Ali Rashid</span></h1>
             <p className="max-w-[500px] mb-9 text-gray-800">I excel at crafting elegant digital experinces and I am proficinet in various programming languages and technologies.</p>
-            <div>
-              <Button>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button variant="outline" size="lg" className="uppercase items-center gap-2" >
                 <span>Download CV</span>
-                < FiDownload/>
+                < FiDownload />
               </Button>
+              <div>social</div>
             </div>
           </div>
           <div>photo</div>

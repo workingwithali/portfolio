c0a98abafa3d0f73d6d8a3777d7d317958ca079b
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-08 13:34:49 +0500

    update

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,9 +1,20 @@
-
-
-const Socials = () => {
+import { icons } from "lucide-react"
+import Link from "next/link"
+import {FaGithub, FaLinkedin, FaYoutube , FaTwitter} from "react-icons/fa"
+const socials =[
+    {icon:<FaGithub/>, path:''},
+    {icon:<FaLinkedin/>, path:''},
+    {icon:<FaYoutube/>, path:''},
+    {icon:<FaTwitter/>, path:''},
+]
+const Socials = ({containerStyle, iconStyle}) => {
   return (
-    <div>
-      Socials
+    <div className="containerStyle">
+      {socials.map((item, index)=>{
+        return (
+        <Link key={index} href={item.path} className="iconStyle">{item.icon}</Link>
+    );
+      })}
     </div>
   )
 }

dff95bf6fdfa56c3495efb329d4e309ef5dfe8ac
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-08 16:25:39 +0500

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Photo from "@/components/Photo"
 import Socials from "@/components/Socials"
 import { Button } from "@/components/ui/button"
 import { FiDownload } from "react-icons/fi"
@@ -21,7 +22,9 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div>photo</div>
+          <div>
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
---
+++ components/Photo.jsx
@@ -0,0 +1,10 @@
+
+const Photo = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Photo

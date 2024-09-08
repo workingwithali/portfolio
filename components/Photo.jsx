4c239c2d8c1b41041d3482c8239bb651ceedc88a
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-08 16:47:30 +0500

    update

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,8 +1,14 @@
-
+'use client';
+import { motion } from "framer-motion"
+import Image from "next/image"
 const Photo = () => {
   return (
-    <div>
-      
+    <div className="w-ful h-ful relative">
+        <motion.div>
+            <motion.div className="w-[298px] h-[298px] xl:w-[]">
+                <Image src="/assets/photo.png" alt="" priority quality={100} fill className="object-contain" />
+            </motion.div>
+        </motion.div>
     </div>
   )
 }

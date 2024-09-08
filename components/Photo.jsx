fcd1393c3c3f8eb2ca8454188a1437c448a435dc
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-08 16:56:04 +0500

    update

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -4,11 +4,15 @@ import Image from "next/image"
 const Photo = () => {
     return (
         <div className="w-ful h-ful relative">
-            <motion.div initial={{opacity:0}} animate={{
-                opacity:1,
-                transition: {delay:2, duration:0.4, ease:"easeIn" }
-            }} > 
-                <motion.div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
+            <motion.div initial={{ opacity: 0 }} animate={{
+                opacity: 1,
+                transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+            }} >
+                <motion.div initial={{ opacity: 0 }} animate={{
+                    opacity: 1,
+                    transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
+                }}
+                    className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
                     <Image src="/assets/photo.png" alt="" priority quality={100} fill className="object-contain" />
                 </motion.div>
             </motion.div>

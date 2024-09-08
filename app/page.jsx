e24485073439ef986b8ca8587d243b6e44350cb7
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-08 12:25:46 +0500

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import { Button } from "@/components/ui/button"
+import { FiDownload } from "react-icons/fi"
 const Home = () => {
   return (
-    <section>home </section>
+    <section className="h-full">
+      <div className="contaniner mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div>
+            
+         </div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   )
 }
 

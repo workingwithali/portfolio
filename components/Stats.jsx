649db01109036bce9f77cedfc8b766a06bbeeb8f
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-09 09:17:47 +0500

    update

---
+++ components/Stats.jsx
@@ -0,0 +1,29 @@
+'use client'
+import CountUp from "react-countup/build/CountUp"
+const stats = [
+    {
+        num:12,
+        text:'Years of Experience'
+    },
+    {
+        num:26,
+        text:'Projects Completed'
+    },
+    {
+        num:8,
+        text:'Technologies Mastered'
+    },
+    {
+        num:500,
+        text:'Code Commits'
+    },
+];
+const Stats = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Stats

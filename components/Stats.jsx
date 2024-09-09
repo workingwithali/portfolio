243dd3d250d74c00f7a62c66bb571c8dd88bdcf3
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-09 09:39:55 +0500

    update

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,5 +1,5 @@
 'use client'
-import CountUp from "react-countup/build/CountUp"
+import CountUp from "react-countup"
 const stats = [
     {
         num:12,
@@ -20,9 +20,23 @@ const stats = [
 ];
 const Stats = () => {
   return (
-    <div>
-      
-    </div>
+    <section>
+        <div className="container mx-auto">
+            <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+                {stats.map((item, index)=>{
+                    return <div key={index}>
+                            <CountUp 
+                            end={item.num}
+                            duration={5}
+                            delay={2}
+                            className="text-4xl xl:text-6xl font-extrabold "
+                            />
+                    </div>
+                    
+                })}
+            </div>
+        </div>
+    </section>
   )
 }
 

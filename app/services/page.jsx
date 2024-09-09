75a9ebf11cf507203837d1ceab8904fd509903f2
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-09 15:24:35 +0500

    update

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
+'use client';
+import {BsArrowDownRight} from "react-icons/bs"
+import Link from "next/link";
+import { motion } from "framer-motion";
+const services =[
+  {
+    num:'01',
+    title:'Web Development',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ipsum in voluptate illo praesentium mollitia. Ipsa iste iure autem.',
+    href:''
+  },
+  {
+    num:'02',
+    title:'Web Development',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ipsum in voluptate illo praesentium mollitia. Ipsa iste iure autem.',
+    href:''
+  },
+  {
+    num:'03',
+    title:'Web Development',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ipsum in voluptate illo praesentium mollitia. Ipsa iste iure autem.',
+    href:''
+  },
+  {
+    num:'04',
+    title:'Web Development',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ipsum in voluptate illo praesentium mollitia. Ipsa iste iure autem.',
+    href:''
+  },
+]
 
 const Services = () => {
   return (
-    <div>
-      services page
-    </div>
+    <section className="min-s-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div 
+        intital={{opacity:0}}
+        animate={{opacity:1, transition:{delay:2.4, duration: 0.4, ease:"easeIn"}}}
+        > 
+        {services.map((service,index)=>{
+          return(
+            <div key={index}>
+              <div>{service.num}</div>
+              <Link href={service.href}>
+              <BsArrowDownRight/>
+              </Link>
+              {/* title */}
+              <h2>{service.title}</h2>
+              {/* description */}
+              <p>{service.description}</p>
+              {/* border */}
+              <div className="bordr-b border-primary "></div>
+            </div>
+          )
+        })}
+        </motion.div>
+      </div>
+    </section>
   )
 }
 

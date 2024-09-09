cf4d6f6d1270419ea35944d833605bfef7f73b56
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-09 09:45:03 +0500

    update

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -24,7 +24,7 @@ const Stats = () => {
         <div className="container mx-auto">
             <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
                 {stats.map((item, index)=>{
-                    return <div key={index}>
+                    return <div className="flex-1 flex gap-4 items-center justify-center xl:justify-start" key={index}>
                             <CountUp 
                             end={item.num}
                             duration={5}

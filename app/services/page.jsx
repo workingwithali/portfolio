4a7a802a87b949896393a18c1481824191016865
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-09 16:00:59 +0500

    update

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -43,7 +43,8 @@ const Services = () => {
               <div key={index}
                 className=" flex-1 flex flex-col justify-center gap-6 group" >
                 <div className="w-full flex justify-between items-center">
-                  <div className="text-5xl font-extrabold">{service.num}</div>
+                  <div className="text-5xl font-extrabold text-outline
+                  text-transparent group-hover:text-outline-hover">{service.num}</div>
                   <Link href={service.href}>
                     <BsArrowDownRight />
                   </Link>

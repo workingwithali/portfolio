4e215b7c75a66e72236509558c974d786fac387f
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-10 23:03:15 +0500

    update

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -3,8 +3,8 @@ import React, {useState}  from 'react'
 import {Swiper, swiperSlide} from 'swiper/react'
 import 'swiper/css';
 import { BsArrowUpRight, BsGithub } from 'react-icons/bs';
-import { Tooltip } from '@/components/ui/tooltip';
-
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import Link from 'next/link';
 const Work = () => {
   return (
     <div>

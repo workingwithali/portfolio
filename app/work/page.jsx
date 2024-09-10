6a9d0cde09cb8b9a6ffda46c8fff7d1fbc45e24b
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-10 23:28:51 +0500

    update

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -5,6 +5,16 @@ import 'swiper/css';
 import { BsArrowUpRight, BsGithub } from 'react-icons/bs';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import Link from 'next/link';
+import { DEFAULT_SERIF_FONT } from 'next/dist/shared/lib/constants';
+const project = [
+  {
+    num: '01',
+    category: 'frontend',
+    title: 'project 1',
+    descrption:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eum a culpa quaerat illum soluta, iure reprehenderit odit eligendi non quod? Quibusdam, error esse.',
+  },
+
+]
 const Work = () => {
   return (
     <div>

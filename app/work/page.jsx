37c05be63d182f9f08dc6aa344d736b8e61828d2
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-11 09:26:51 +0500

    update

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -7,7 +7,7 @@ import { BsArrowUpRight, BsGithub } from 'react-icons/bs';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import Link from 'next/link';
 import { DEFAULT_SERIF_FONT } from 'next/dist/shared/lib/constants';
-const project = [
+const projects = [
   {
     num: '01',
     category: 'frontend',
@@ -47,14 +47,31 @@ const project = [
 
 ]
 const Work = () => {
-  const [project, setProject] = useState(project[0]);
+  const [project, setProject] = useState(projects[0]);
   return (
     <motion.div
     initial={{opacity:0}}
     animate={{opacity:1}}
     className='min-h-[80vh] flex flex-col justify-center py-12 xl:px-0'
     >
-      <div className="container mx-auto">project</div>
+      <div className="container mx-auto">
+        <div className='flex flex-col xl:flew-row xl:gap-[30px]'>
+          <div className='w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none '>
+            <div>
+              {/* outline */}
+              <div className='text-8xl leading-none font-extrabold text-transparent  text-outline'>{project.num}</div>
+              {/* category */}
+              <h2 className='text-[42px] font-bold leading-none text-primary group-hover:text-accent-hover translate-all duration-500 capitalize'>{project.category} Project</h2>
+              {/* descrption */}
+              <p className='text-primary/60'>{project.descrption}</p>
+              <ul className='flex gap-4'>{project.stack.map((item, index)=>{
+                return <li key={index} className='text-xl '>{item.name}</li>
+              })}</ul>
+            </div>
+          </div>
+          <div className='w-full xl:w-[50%]'>slider</div>
+        </div>
+      </div>
     </motion.div>
 
   )

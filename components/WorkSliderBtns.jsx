248a6c60731d9b8b03ae9faf21a7080f6080b25c
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-11 13:18:53 +0500

    update

--- components/WorkSliderBtns.jsx
+++ components/WorkSliderBtns.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useSwiper } from "swiper/react";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
-const WorkSliderBtns = () => {
+const WorkSliderBtns = ({ containerStyles, btnStyles, iconsStyles }) => {
+  const swiper = useSwiper();
   return (
-    <div>
-      
+    <div className={containerStyles}>
+      <button className={btnStyles} onClick={() => swiper.slidePrev()}>
+        <FiChevronLeft className={iconsStyles} />
+      </button>
+      <button className={btnStyles} onClick={() => swiper.slideNext()}>
+        <FiChevronRight className={iconsStyles} />
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default WorkSliderBtns
+export default WorkSliderBtns;

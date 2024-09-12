eb1a188f1a2958a82aba064ad183964f24bcd5ec
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-12 19:56:14 +0500

    update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,38 @@
 'use client'
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "03326275882",
+  },
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "03326275882",
+  },
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "03326275882",
+  },
+];
+
 const Contact = () => {
   return (
     <div>

b923b1120d84490fc78789e37a62df58c427c796
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-13 18:09:27 +0500

    update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -32,6 +32,10 @@ const info = [
     description: "Pakistan",
   },
 ];
+const textAnimation = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeOut" } },
+};
 
 const Contact = () => {
   return (

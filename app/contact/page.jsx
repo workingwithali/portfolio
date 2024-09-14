75a83be48cb762085c2f71884221ed2e8b954475
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-14 10:55:02 +0500

    update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -18,21 +18,22 @@ import { motion } from "framer-motion";
 
 const info = [
   {
-    icon: <FaPhoneAlt />,
+    icon: <FaPhoneAlt className="text-primary" />,
     title: "Phone",
     description: "03326275882",
   },
   {
-    icon: <FaEnvelope />,
+    icon: <FaEnvelope className="text-primary" />,
     title: "Email",
     description: "Youremail@gamil.com",
   },
   {
-    icon: <FaMapMarkerAlt />,
-    title: "Adress",
+    icon: <FaMapMarkerAlt className="text-primary" />,
+    title: "Address",
     description: "Pakistan",
   },
 ];
+
 const textAnimation = {
   hidden: { opacity: 0, y: 50 },
   visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeOut" } },
@@ -40,24 +41,47 @@ const textAnimation = {
 
 const Contact = () => {
   return (
-    <motion.section className="py-6" initial="hidden" animate="visible">
-
-      <motion.div
-        variants={textAnimation}
-        className="container mx-auto "
-      >
-        <div className="flex flex-col xl:flex-row gap-[30px] ">
+    <motion.section className="py-6 bg-black" initial="hidden" animate="visible">
+      <motion.div variants={textAnimation} className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
-          <div>
-            <form>form</form>
+          <div className=" xl:h-[54%] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-gray-800 rounded-xl">
+              <h3 className="text-4xl font-semibold text-accent">Let's Work Together</h3>
+              <p className="text-primary">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta necessitatibus architecto voluptatem?
+              </p>
+              {/* input */}
+              <div className="grid grid-cols-1 xl:grid-cols-2 gap-6">
+                <Input type="text" placeholder="First Name" className="bg-black text-primary border-accent focus:border-hover" />
+                <Input type="text" placeholder="Last Name" className="bg-black text-primary border-accent focus:border-hover" />
+                <Input type="email" placeholder="Email Address" className="bg-black text-primary border-accent focus:border-hover" />
+                <Input type="phone" placeholder="Phone Number" className="bg-black text-primary border-accent focus:border-hover" />
+              </div>
+              <Textarea placeholder="Your Message" className="bg-black text-primary border-accent focus:border-hover" />
+              <Button variant="outline" className="uppercase bg-accent text-black hover:bg-hover">
+                Send Message
+              </Button>
+            </form>
           </div>
           {/* info */}
-          <div>info</div>
+          <div className="flex-1 flex items-center xl:justify-center order-1 xl:order-none mb-8 xl:mb-0">
+            <div className="text-primary space-y-6">
+              {info.map((item, index) => (
+                <div key={index} className="flex items-center space-x-4">
+                  <div className="text-xl">{item.icon}</div>
+                  <div>
+                    <h4 className="text-accent">{item.title}</h4>
+                    <p className="text-gray-800">{item.description}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </motion.div>
-      
     </motion.section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;

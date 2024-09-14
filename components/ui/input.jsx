4ec0a7cf5189f461391e2d39202eaf516b0b2af9
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-14 10:58:22 +0500

    update

--- components/ui/input.jsx
+++ components/ui/input.jsx
@@ -4,14 +4,15 @@ import { cn } from "@/lib/utils"
 
 const Input = React.forwardRef(({ className, type, ...props }, ref) => {
   return (
-    (<input
+    <input
       type={type}
       className={cn(
-        "flex h-10 w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
+        "flex h-10 w-full rounded-md border border-accent bg-black px-3 py-2 text-sm ring-offset-black file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-800 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-accent dark:bg-black dark:ring-offset-black dark:placeholder:text-gray-800 dark:focus-visible:ring-hover",
         className
       )}
       ref={ref}
-      {...props} />)
+      {...props}
+    />
   );
 })
 Input.displayName = "Input"

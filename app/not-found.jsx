cd61d3da3c9aecd2a5f65328f2ec3731fc74bb8c
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-15 20:58:28 +0500

    update

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Link from 'next/link';
-import "./not-found.css"; // External CSS for CRT effect
+import "./not-found.css";
 
 const NotFound = () => {
     return (

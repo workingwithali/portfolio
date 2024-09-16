bae71587d69317090dd681a197ebff120fdd427d
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-16 13:46:00 +0500

    update

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,5 +1,12 @@
 "use client"
 import {FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs,} from "react-icons/fa"
+import {SiTailwindcss, SiNodedotjs} from "react-icons/si"
+
+const about ={
+  title: "about me",
+  description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum commodi veritatis explicabo."
+}
+
 const Resume = () => {
   return (
     <div>

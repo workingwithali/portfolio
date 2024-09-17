6aaab27d361fce7c7175529c04446056f6c64421
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-17 17:44:22 +0500

    update

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,10 +1,44 @@
 "use client"
-import {FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs,} from "react-icons/fa"
-import {SiTailwindcss, SiNodedotjs} from "react-icons/si"
+import { FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs, } from "react-icons/fa"
+import { SiTailwindcss, SiNodedotjs } from "react-icons/si"
 
-const about ={
+const about = {
   title: "about me",
-  description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum commodi veritatis explicabo."
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum commodi veritatis explicabo.",
+  info: [
+    {
+      fieldName: 'Name',
+      fieldValue: 'Ali Rashid'
+    },
+    {
+      fieldName: 'Phone',
+      fieldValue: '03326275882'
+    },
+    {
+      fieldName: 'Experience',
+      fieldValue: '12+ Year'
+    },
+    {
+      fieldName: 'Nationality',
+      fieldValue: 'Pakistani'
+    },
+    {
+      fieldName: 'Skype',
+      fieldValue: 'Ali Rashid'
+    },
+    {
+      fieldName: 'Email',
+      fieldValue: 'alirashid@email.com'
+    },
+    {
+      fieldName: 'Freelance',
+      fieldValue: 'Available'
+    },
+    {
+      fieldName: 'Language',
+      fieldValue: 'English, Urdu'
+    },
+  ]
 }
 
 const Resume = () => {

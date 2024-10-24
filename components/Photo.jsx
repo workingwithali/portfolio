113399fb574b397cd52b5ec23a0bdc69f2f96b54
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-24 16:15:05 +0500

    update

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -23,7 +23,7 @@ const Photo = () => {
                             priority
                             quality={100}
                             fill
-                            className="object-contain filter contrast-150 brightness-75 grayscale glitch z-10 rounded-full "
+                            className="object-contain filter contrast-150 brightness-75 grayscale glitch z-10 rounded-full bg-gray-700"
                         />
                     </motion.div>
                     

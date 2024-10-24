ff159f80088320c9d7d41400effb1b04ea8e27df
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-24 16:02:01 +0500

    update

--- components/ali.jsx
+++
@@ -1,8 +0,0 @@
-<Image
-    src="/assets/photo.png"
-    alt=""
-    priority
-    quality={100}
-    fill
-    className="object-contain filter contrast-150 brightness-75 grayscale glitch z-10 rounded-full"
-/>
